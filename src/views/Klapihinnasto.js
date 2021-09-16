99ad03762a067317fe49da1d60c0c3e51ca18932
lmp20 <leena.poikolainen20@imperial.ac.uk>
2021-09-16 18:24:00 +0100

    adjusted klapierat padding

--- src/views/Klapihinnasto.js
+++ src/views/Klapihinnasto.js
@@ -7,15 +7,14 @@ export default function Klapihinnasto() {
     return (
         <>
             <Navbar />
-           
-            
+                
             <div className="pt-20 pb-32 ">
-                <div class="flex justify-center">
+                <div class="pl-7 justify-start">
                     <h3 className="text-3xl mb-2 font-semibold leading-normal">
                         Klapierät
                     </h3>
                 </div>
-                <div class="flex justify-center"> 
+                <div class="pl-7 justify-start"> 
                     <iframe height="500" width="90%" className=""
                         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT6x1GkXEvrfbmHIJoPZAXp4wxiRX1ANqOk_E-ybGTS56Cgh1R1UPBH96o8FFQXhKn8Vt-cnIH_lnuA/pubhtml?gid=0&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=A1:B26"
                     >
